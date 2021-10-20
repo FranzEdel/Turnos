78aef91322d97875135cc31680b630c3d2f48833
franzedel <franz.t77@gmail.com>
2021-10-19 21:48:16 -0400

    Frontend Medicos Part1

--- scripts.sql
+++ scripts.sql
@@ -1,12 +1,16 @@
 SELECT * FROM Especialidades;
 
+DELETE FROM Especialidades;
+
 INSERT INTO Especialidades VALUES('Cardiología'),
                                  ('Cirugía'),
                                  ('Medicina General'),
                                  ('Medicina Interna');
 
 SELECT * FROM Medicos;
-INSERT INTO Medicos VALUES('Juan','Lopez','Junin #100','11223344','juan@mail.com','2021/09/28 08:00','2021/09/28 18:00');
+INSERT INTO Medicos VALUES('Juan','Lopez','Junin #100','11223344','juan@mail.com','2021/09/28 08:00','2021/09/28 18:00',2),
+('Maria','Perez','Loa #100','55443322','maria@mail.com','2021/10/28 08:00','2021/10/28 18:00',4),
+('Javier','Barrero','Av Jaime Mendoza #100','11223344','javier@mail.com','2021/09/28 08:00','2021/09/28 18:00',3);
 
 SELECT * FROM Especialidades e JOIN Medicos m ON e.IdEspecialidad = m.IdMedico
 WHERE m.IdMedico = 3
