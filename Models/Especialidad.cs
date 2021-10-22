13cf1ad7d2dd2a0c71a6dde75eab46a05b0d8d2a
franzedel <franz.t77@gmail.com>
2021-10-21 21:32:23 -0400

    Frontend Medicos y DataAnnotations

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -7,6 +7,10 @@ namespace Turnos.Models
     {
         [Key]
         public int IdEspecialidad { get; set; }
+
+        [StringLength(200, ErrorMessage = "La descripción debe tener como máximo 200 caracteres")]
+        [Required(ErrorMessage = "Debe ingresar una descripción")]
+        [Display(Name = "Descripción", Prompt = "Ingrese una descripción")]
         public string Descripcion { get; set; }
 
         public ICollection<Medico> MedicoLista { get; set; }
